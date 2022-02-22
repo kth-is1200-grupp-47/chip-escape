314f36388ecb54071ba4495b0500d437344c28cc
Hannes Mann <hannesmann2000@gmail.com>
2022-02-22 23:44:28 +0100

    Fix incorrect overlay image function

--- hw/display_draw.c
+++ hw/display_draw.c
@@ -20,19 +20,19 @@ void display_overlay_image(Image image, int x, int y) {
 }
 
 void display_draw_image_region(Image image, int x, int y, int rx, int ry, int rw, int rh) {
-	/* Clear all bits behind the image */
-	display_draw_rectangle(false, x, y, rw, rh);
-	display_overlay_image_region(image, x, y, rx, ry, rw, rh);
-}
-
-void display_overlay_image_region(Image image, int x, int y, int rx, int ry, int rw, int rh) {
-	/*
-	 * Iterate through y in the outer loop for the best cache efficiency.
-	 * All writes will be done to the same page for eight pixels, so this makes use of spaital locality.
-	 */
 	for(int yp = y; yp < y + rh; yp++) {
 		for(int xp = x; xp < x + rw; xp++) {
 			display_write_bit(image_bit(image, xp - x + rx, yp - y + ry), xp, yp);
+		}
+	}
+}
+
+void display_overlay_image_region(Image image, int x, int y, int rx, int ry, int rw, int rh) {
+	for(int yp = y; yp < y + rh; yp++) {
+		for(int xp = x; xp < x + rw; xp++) {
+			if(image_bit(image, xp - x + rx, yp - y + ry)) {
+				display_write_bit(true, xp, yp);
+			}
 		}
 	}
 }
