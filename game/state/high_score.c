b6fc24e66c9769575a4abb2ebeec4d06d9eef611
botani <botani@botanis-MBP.lan>
2022-03-02 22:44:10 +0100

    Changed for top 5 to top 4

--- game/state/high_score.c
+++ game/state/high_score.c
@@ -4,9 +4,9 @@
 #include "hw/inputs.h"
 
 /* Array for initials and scores */
-char initials[15];
-int scores[5];
-int data_array[21];
+char initials[12];
+int scores[4];
+int data_array[17];
 
 /* This function converts hex to char */
 char hex2char(int hex){
@@ -22,7 +22,7 @@ char hex2char(int hex){
 void high_score_load(const int* data){
 
     /* copy data to an array so that we can save them for later */
-    for(int i = 0; i < 21; i++){
+    for(int i = 0; i < 17; i++){
         data_array[i] = data[i];
     }
 
@@ -30,7 +30,8 @@ void high_score_load(const int* data){
     int count_scores = 0;
 
     /* data begins with a 0 element */
-    for(int i = 1; i < 21; i++){
+    for(int i = 1; i < 17; i++){
+        
         /* Every fourth character is a score */
         if(i%4 == 0){
             scores[count_scores] = data[i];
@@ -57,9 +58,9 @@ void high_score_draw() {
 }
 
 /* Returns true if the score acquired is a high score */
-bool is_top_5(int score){
+bool is_top_4(int score){
 
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < 4; i++){
         if(scores[i] < score){
             return true;
         }
@@ -70,10 +71,13 @@ bool is_top_5(int score){
     }
 }
 
-/* This function adds a high score to the list */
+/************************************************
+ * This function adds a high score to the list  *
+ * and saves the new high score to the EEPROM.  *
+ ************************************************/
 void high_score_put_n_save(int data[]){
 
-    for(int i = 4; i < 21; i = i + 4){
+    for(int i = 4; i < 17; i = i + 4){
         if(data_array[i] < data[3]){
             for(int j = 0; j <= 3; j--){
                 data_array[i-j] = data[3-j];
@@ -82,5 +86,8 @@ void high_score_put_n_save(int data[]){
         }
     }
 
-    /* TODO: save to EEPROM */
+    /* Saves to EEPROM */
+    for(int i = 0; i < 17; i++){
+        transfer_i2c(data_array[i]);
+    }
 }
