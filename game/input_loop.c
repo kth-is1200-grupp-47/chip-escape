d5f317777d274ac3f3b28c657c31784f81e24c5c
botani <botani@botanis-MacBook-Pro.local>
2022-03-02 13:12:29 +0100

    Changed input_loop.c

--- game/input_loop.c
+++ game/input_loop.c
@@ -12,43 +12,24 @@
 void loop(){
 
     /* Variable for pushed buttons */
-    int pushed_buttons = getBtns();
-
-    /* Variable for temporary state */
-    GameState temp_state;
-
-    
+    int pushed_button = getBtns();
 
     switch(current_state){
         case STATE_MAIN_MENU:
-
-            /* Pointer for main_menu_options */
-            char *m_ptr = &main_menu_options;
-
-            /* Marks first option */
-            temp_state = options2state(*m_ptr);
             
             /************************************************************ 
              * If the game is in the main menu and buttons are pressed  *
              * options are marked temporarily                           * 
              ************************************************************/
 
-            /* Right movement */
-            if(pushed_buttons & 0b0010){
-                /* Check which option is marked so that it doesn't go past available options */
-                if(temp_state != STATE_HIGH_SCORE){
-                    ptr++;
-                    temp_state = options2state(*m_ptr);
-                }
+            /* High score */
+            if(pushed_button & 0b0010){
+                current_state = STATE_HIGHSCORE_LIST;
             }
 
-            /* Left movement */
+            /* Play */
             if(pushed_buttons & 0b1000){
-                /* Check which option is marked so that it doesn't go past available options */
-                if(temp_state != STATE_LEVEL){
-                    ptr--;
-                    temp_state = options2state(*m_ptr);
-                }
+                current_state = STATE_LEVEL;
             }
         
         /***************************************************************
@@ -57,36 +38,32 @@ void loop(){
         case STATE_LEVEL:
             /* JUMP movement */
             if(pushed_buttons & 0b0001){
-                
+                /* TODO jump */
             }
 
             /* RIGHT movement */
             if(pushed_buttons & 0b0010){
-                
+                /* TODO RIGHT move */
             }
 
-            /* LOAD GAME MENU */
+            /* Exit to MAIN MENU */
             if(pushed_buttons & 0b0100){
-                temp_state = STATE_GAME_MENU;
+                current_state = STATE_MAIN_MENU;
             }
 
             /* LEFT movement */
             if(pushed_buttons & 0b1000){
-                
+                /* TODO Left move */
             }
         
         /* Only option at HIGH_SCORE_LIST is to go back to main menu */
         case STATE_HIGH_SCORE_LIST:
             
             if(pushed_buttons & 0b1000){
-                /* TODO: Back to main menu */
+                current_state = STATE_MAIN_MENU;
             }
 
         
     }
-
-        
-
-
 
 }
